2ba3cbd2a99a6bcb04a9ade1b2b822e6ae39c8c6
Olehovych <olehbolonnuj@gmail.com>
2023-02-14 23:49:28 +0200

    first

---
+++ main.js
@@ -0,0 +1,66 @@
+// 1. Написати функцію, яка приймає 2 числа та повертає -1, якщо перше
+// менше, ніж друге; 1 - якщо перше більше, ніж друге; та 0 – якщо числа
+// рівні.
+
+// function receiving(params) {
+//   let firstNumber = prompt("Write firs number");
+//   let secondNumber = prompt("Write second number");
+
+//   if (firstNumber < secondNumber) {
+//     alert("-1");
+//   } else {
+//     if (firstNumber > secondNumber) {
+//       alert("1");
+//     }
+//     if (firstNumber == secondNumber) {
+//       alert("0");
+//     }
+//   }
+// }
+// receiving();
+
+// 2. Написати функцію, яка обчислює факторіал переданого їй числа.
+
+// function factorial(n) {
+//   if (n == 1) {
+//     return 1;
+//   } else {
+//   }
+//   return n * factorial(n - 1);
+// }
+
+// let userNum = prompt("Write factorial");
+// alert(factorial(userNum));
+
+// 3. Написати функцію, яка приймає три окремі цифри та перетворює їх на
+// одне число. Наприклад: цифри 1, 4, 9 перетворяться на число 149.
+
+// function threeNumber() {
+//   let numberFirst = prompt("Write first number");
+//   let numberSecond = prompt("Write second number");
+//   let numberThird = prompt("Write third number");
+
+//   alert(numberFirst + numberSecond + numberThird);
+// }
+// threeNumber();
+
+// 4. Написати функцію, яка приймає довжину та ширину прямокутника та
+// обчислює його площу. Якщо функцію передали 1 параметр, вона
+// обчислює площу квадрата.
+
+// function area() {
+//   let length = prompt("Write length");
+//   let width = prompt("Write width");
+
+//   const area = length * width;
+//   alert(area);
+// }
+// area();
+
+// 5. Написати функцію, яка перевіряє, чи є передане їй число досконалим.
+// Досконале число – це число, що дорівнює сумі всіх своїх дільників.
+
+// 6. Написати функцію, яка приймає мінімальне та максимальне значення
+// для діапазону, та виводить лише ті числа з діапазону, які є досконалими.
+// Використовуйте написану раніше функцію, щоб дізнаватися, чи
+// досконале число, чи ні
